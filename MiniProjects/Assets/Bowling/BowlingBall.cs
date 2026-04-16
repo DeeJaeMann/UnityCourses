4d25716597445fb4c99fcc32a5b00934ab0e6db9
Dee J. Mann <dee.j.mann@hotmail.com>
2026-04-16 10:01:11 -0400

    Completed BowlingBall

--- MiniProjects/Assets/Bowling/BowlingBall.cs
+++ MiniProjects/Assets/Bowling/BowlingBall.cs
@@ -10,6 +10,19 @@ public class BowlingBall : MonoBehaviour
 
     public void Bowl()
     {
-        Debug.Log("Bowl");
+        // Debug.Log("Bowl");
+        rig.AddForce(Vector3.forward * bowlForce, ForceMode.Impulse);
+    }
+
+    public void MoveLeft()
+    {
+        // Debug.Log("MoveLeft");
+        if(transform.position.x > leftBarrier) transform.position += Vector3.left * moveIncrement;
+    }
+
+    public void MoveRight()
+    {
+        // Debug.Log("MoveRight");
+        if(transform.position.x < rightBarrier) transform.position += Vector3.right * moveIncrement;
     }
 }
