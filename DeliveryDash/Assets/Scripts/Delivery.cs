ff0b939e4ca7e4b8dc2ee2e4cb1ddb2d2959cf82
Dee J. Mann <dee.j.mann@hotmail.com>
2026-08-26 08:59:07 -0400

    Added package prefab and pickup logic

--- DeliveryDash/Assets/Scripts/Delivery.cs
+++ DeliveryDash/Assets/Scripts/Delivery.cs
@@ -3,18 +3,20 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
-    private bool hasPackage;
+    private bool _hasPackage;
+    [SerializeField] private float delay = 0.5f;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Package"))
+        if (other.CompareTag("Package") && !_hasPackage)
         {
             Debug.Log($"Package Trigger {other.name}");
-            hasPackage = true;
+            Destroy(other.gameObject, delay);
+            _hasPackage = true;
         }
-        else if (other.CompareTag("Customer") && hasPackage)
+        else if (other.CompareTag("Customer") && _hasPackage)
         {
             Debug.Log($"Customer Delivered {other.name}");
-            hasPackage = false;
+            _hasPackage = false;
         }
     }
 }
